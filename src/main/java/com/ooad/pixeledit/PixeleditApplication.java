02d9bb828e646f3a22c37d2ed41278a2293b0465
legbing <spoorthikalkunte@gmail.com>
2023-04-07 21:31:55 +0530

    Added initial files

---
+++ src/main/java/com/ooad/pixeledit/PixeleditApplication.java
@@ -0,0 +1,13 @@
+package com.ooad.pixeledit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixeleditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixeleditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ooad/pixeledit/PixeleditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ooad.pixeledit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixeleditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
