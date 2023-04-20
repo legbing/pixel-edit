d6bc521bf0e31d89ee441f5254cdfe78925c1f22
thrupthi2002 <96109514+thrupthi2002@users.noreply.github.com>
2023-04-20 12:29:35 +0530

    updated image upload

--- src/main/java/com/ooad/pixeledit/service/FilesStorageServiceImpl.java
+++ src/main/java/com/ooad/pixeledit/service/FilesStorageServiceImpl.java
@@ -1,5 +1,5 @@
 package com.ooad.pixeledit.service;
-
+import com.ooad.pixeledit.entity.Image;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.FileAlreadyExistsException;
@@ -7,17 +7,21 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
+import com.ooad.pixeledit.repository.ImageRepository;
 
 @Service
 public class FilesStorageServiceImpl implements FilesStorageService {
   private final Path root = Paths.get("./uploads");
-
+  @Autowired
+  private ImageRepository imageRepo;
+  
   @Override
   public void init() {
     System.out.println(root);
@@ -33,6 +37,13 @@ public class FilesStorageServiceImpl implements FilesStorageService {
   public void save(MultipartFile file) {
     try {
       Files.copy(file.getInputStream(), this.root.resolve(file.getOriginalFilename()));
+      Image image=new Image();
+      image.setName(file.getOriginalFilename());
+      image.setUrl(root+"\\"+ file.getOriginalFilename());
+      org.springframework.security.core.Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+      System.out.println(auth.getName());
+      image.setAuthor(auth.getName());
+      imageRepo.save(image);
     } catch (Exception e) {
       if (e instanceof FileAlreadyExistsException) {
         throw new RuntimeException("A file of that name already exists.");
@@ -82,3 +93,4 @@ public class FilesStorageServiceImpl implements FilesStorageService {
     }
   }
 }
+
