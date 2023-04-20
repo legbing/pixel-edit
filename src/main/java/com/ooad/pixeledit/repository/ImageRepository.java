2acaecd342cb0acd65e9880dcd6f3c5d67fa0ac2
thrupthi2002 <96109514+thrupthi2002@users.noreply.github.com>
2023-04-20 12:24:32 +0530

    updated image upload

---
+++ src/main/java/com/ooad/pixeledit/repository/ImageRepository.java
@@ -0,0 +1,9 @@
+package com.ooad.pixeledit.repository;
+import com.ooad.pixeledit.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+    Optional<Image> findByName(String name);
+}
